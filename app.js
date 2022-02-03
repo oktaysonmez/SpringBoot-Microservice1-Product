7c491b2ff5baa59e2bdb84f7e44bbab6c3b30168
oktaysonmez <oktaysonmez@gmail.com>
2022-02-03 17:04:13 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,9 +1,1 @@
-#!/usr/bin/env node
-var debug = require('debug')('toto');
-var app = require('../app');
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function() {
-  debug('Express server listening on port ' + server.address().port);
-});
+.listen(process.env.PORT || 5000)
