097f090ebdda2d2afe0851eacaf95ad798fa7226
oktaysonmez <oktaysonmez@gmail.com>
2022-02-03 16:14:02 +0300

    initial commit

--- src/main/java/com/sha/product/security/SecurityConfig.java
+++ src/main/java/com/sha/product/security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-//@EnableWebSecurity //web tabanli guvenlik saglamak icin
+@EnableWebSecurity //web tabanli guvenlik saglamak icin
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
     @Value("${service.security.secure-key-username}")
